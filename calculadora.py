46199bb912adbde718201329d7a9cf8fc7699631
DANIEL_LADINO <danielladino222@unisangil.edu.co>
2026-09-24 19:26:55 -0500

    Funcionalidades de los operadores, solo en la escritura

--- calculadora.py
+++ calculadora.py
@@ -1,134 +1,138 @@
-import funciones
+import funciones as fn
 import tkinter as tk
 
 # Variables
 
-texto_label = ''
+text_label = ""
 
 # ventana
 
 win = tk.Tk()
-win.title('Calculadora basica')
-win.geometry('300x300')
+win.title("Calculadora basica")
+win.geometry("300x300")
 
 # Cuerpo de la ventana
 
 # Label que resive los valores
 
-label = tk.Label(win, font=('Arial', 20), justify='right', text=texto_label)
-label.grid(row=0,  column=0, columnspan=4, rowspan=2, padx=10,
-           pady=10, sticky='SNEW')
+label = tk.Entry(win, font=("Arial", 20), justify="right", text=text_label)
+label.grid(row=0, column=0, columnspan=4, rowspan=2, padx=10, pady=10, sticky="SNE")
 
 
 def Presionar(simbolo):
-    label['text'] += simbolo
+
+    global text_label
+
+    label.delete(0, tk.END)
+    text_label = fn.logica_precionar(simbolo)
+    label.insert(0, text_label)
 
 
 # Botones
 
 # Raiz
 
-btn_root = tk.Button(win, text='√', command=lambda: Presionar('√'))
-btn_root.grid(row=3, column=0, padx=5, pady=5, sticky='SNEW')
+btn_root = tk.Button(win, text="√", command=lambda: Presionar("√"))
+btn_root.grid(row=3, column=0, padx=5, pady=5, sticky="SNEW")
 
 # Porcentaje
 
-btn_percentage = tk.Button(win, text='%')
-btn_percentage.grid(row=3, column=1, padx=5, pady=5, sticky='SNEW')
+btn_percentage = tk.Button(win, text="%", command=lambda: Presionar("%"))
+btn_percentage.grid(row=3, column=1, padx=5, pady=5, sticky="SNEW")
 
 # Borrar
 
-btn_delete = tk.Button(win, text='⌫')
-btn_delete.grid(row=3, column=2, padx=5, pady=5, sticky='SNEW')
+btn_delete = tk.Button(win, text="⌫", command=lambda: Presionar("⌫"))
+btn_delete.grid(row=3, column=2, padx=5, pady=5, sticky="SNEW")
 
 # Dividir
 
-btn_split = tk.Button(win, text='÷')
-btn_split.grid(row=3, column=3, padx=5, pady=5, sticky='SNEW')
+btn_split = tk.Button(win, text="÷", command=lambda: Presionar("÷"))
+btn_split.grid(row=3, column=3, padx=5, pady=5, sticky="SNEW")
 
 # Ciete
 
-btn_seven = tk.Button(win, text='7')
-btn_seven.grid(row=4, column=0, padx=5, pady=5, sticky='SNEW')
+btn_seven = tk.Button(win, text="7", command=lambda: Presionar("7"))
+btn_seven.grid(row=4, column=0, padx=5, pady=5, sticky="SNEW")
 
 # Ocho
 
-btn_eight = tk.Button(win, text='8')
-btn_eight.grid(row=4, column=1, padx=5, pady=5, sticky='SNEW')
+btn_eight = tk.Button(win, text="8", command=lambda: Presionar("8"))
+btn_eight.grid(row=4, column=1, padx=5, pady=5, sticky="SNEW")
 
 # Nueve
 
-btn_nine = tk.Button(win, text='9')
-btn_nine.grid(row=4, column=2, padx=5, pady=5, sticky='SNEW')
+btn_nine = tk.Button(win, text="9", command=lambda: Presionar("9"))
+btn_nine.grid(row=4, column=2, padx=5, pady=5, sticky="SNEW")
 
 # Multiplicar
 
-btn_multiply = tk.Button(win, text='X')
-btn_multiply.grid(row=4, column=3, padx=5, pady=5, sticky='SNEW')
+btn_multiply = tk.Button(win, text="X", command=lambda: Presionar("X"))
+btn_multiply.grid(row=4, column=3, padx=5, pady=5, sticky="SNEW")
 
 # Cuatro
 
-btn_four = tk.Button(win, text='4')
-btn_four.grid(row=4, column=0, padx=5, pady=5, sticky='SNEW')
+btn_four = tk.Button(win, text="4", command=lambda: Presionar("4"))
+btn_four.grid(row=5, column=0, padx=5, pady=5, sticky="SNEW")
 
 # Cinco
 
-btn_five = tk.Button(win, text='5')
-btn_five.grid(row=4, column=1, padx=5, pady=5, sticky='SNEW')
+btn_five = tk.Button(win, text="5", command=lambda: Presionar("5"))
+btn_five.grid(row=5, column=1, padx=5, pady=5, sticky="SNEW")
 
 # Seis
 
-btn_six = tk.Button(win, text='6')
-btn_six.grid(row=4, column=2, padx=5, pady=5, sticky='SNEW')
+btn_six = tk.Button(win, text="6", command=lambda: Presionar("6"))
+btn_six.grid(row=5, column=2, padx=5, pady=5, sticky="SNEW")
 
 # resta
 
-btn_subtract = tk.Button(win, text='-')
-btn_subtract.grid(row=4, column=3, padx=5, pady=5, sticky='SNEW')
+btn_subtract = tk.Button(win, text="-", command=lambda: Presionar("-"))
+btn_subtract.grid(row=5, column=3, padx=5, pady=5, sticky="SNEW")
 
 # Uno
 
-btn_one = tk.Button(win, text='1')
-btn_one.grid(row=5, column=0, padx=5, pady=5, sticky='SNEW')
+btn_one = tk.Button(win, text="1", command=lambda: Presionar("1"))
+btn_one.grid(row=6, column=0, padx=5, pady=5, sticky="SNEW")
 
 # Dos
 
-btn_two = tk.Button(win, text='2')
-btn_two.grid(row=5, column=1, padx=5, pady=5, sticky='SNEW')
+btn_two = tk.Button(win, text="2", command=lambda: Presionar("2"))
+btn_two.grid(row=6, column=1, padx=5, pady=5, sticky="SNEW")
 
 # Three
 
-btn_three = tk.Button(win, text='3')
-btn_three.grid(row=5, column=2, padx=5, pady=5, sticky='SNEW')
+btn_three = tk.Button(win, text="3", command=lambda: Presionar("3"))
+btn_three.grid(row=6, column=2, padx=5, pady=5, sticky="SNEW")
 
 # Suma
 
-btn_add = tk.Button(win, text='+')
-btn_add.grid(row=5, column=3, padx=5, pady=5, sticky='SNEW')
+btn_add = tk.Button(win, text="+", command=lambda: Presionar("+"))
+btn_add.grid(row=6, column=3, padx=5, pady=5, sticky="SNEW")
 
 # Elevado
 
-btn_high = tk.Button(win, text='^')
-btn_high.grid(row=6, column=0, padx=5, pady=5, sticky='SNEW')
+btn_high = tk.Button(win, text="^", command=lambda: Presionar("^"))
+btn_high.grid(row=7, column=0, padx=5, pady=5, sticky="SNEW")
 
 # Cero
 
-btn_zero = tk.Button(win, text='0')
-btn_zero.grid(row=6, column=1, padx=5, pady=5, sticky='SNEW')
+btn_zero = tk.Button(win, text="0", command=lambda: Presionar("0"))
+btn_zero.grid(row=7, column=1, padx=5, pady=5, sticky="SNEW")
 
 # Punto
 
-btn_point = tk.Button(win, text='.')
-btn_point.grid(row=6, column=2, padx=5, pady=5, sticky='SNEW')
+btn_point = tk.Button(win, text=".", command=lambda: Presionar("."))
+btn_point.grid(row=7, column=2, padx=5, pady=5, sticky="SNEW")
 
 # Igual
 
-btn_equals = tk.Button(win, text='=')
-btn_equals.grid(row=6, column=3, padx=5, pady=5, sticky='SNEW')
+btn_equals = tk.Button(win, text="=", command=lambda: Presionar("="))
+btn_equals.grid(row=7, column=3, padx=5, pady=5, sticky="SNEW")
 
 for i in range(4):
     win.columnconfigure(i, weight=1)
-for i in range(7):
+for i in range(8):
     win.rowconfigure(i, weight=1)
 
 win.mainloop()
--- funciones.py
+++ funciones.py
@@ -1,14 +1,80 @@
 datos = list()
 operadores = list()
+texto = ""
+numeros = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+operadores = ("√", "%", "÷", "X", "-", "+", "^", ".")
 
 
-def Presionar(text):
+def validacion_operador(simbolo):
 
-    global datos
+    global texto
 
-    if datos.len() == 0:
+    if len(texto) == 0:
 
-        text += "√"
-        operadores.append("√")
+        texto = "0"
+        texto += simbolo
 
-    return text
+    elif texto[-1] in operadores:
+        texto = texto[:-1] + simbolo
+
+    else:
+        texto += simbolo
+
+
+def logica_precionar(simbolo):
+
+    global texto
+
+    if simbolo == "√":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "%":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "⌫":
+
+        if len(texto) > 0:
+
+            texto = texto[:-1]
+
+    elif simbolo == "÷":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "X":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "-":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "+":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "^":
+
+        validacion_operador(simbolo)
+
+    elif simbolo == "=":
+
+        print("Condiciones de ")
+
+    elif simbolo == ".":
+
+        if len(texto) == 0:
+
+            texto = "0."
+
+        if texto[-1] in numeros:
+
+            texto += "."
+
+    else:
+
+        texto += simbolo
+
+    return texto
